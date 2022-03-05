904c77abd92f56d5d7bdb7cd029f089cd13d14f5
liquidpeachy <84338514+liquidpeachy@users.noreply.github.com>
2022-03-05 17:28:32 +0000

    Update __init__.py

--- advertools2/__init__.py
+++ advertools2/__init__.py
@@ -5,25 +5,25 @@ __author__ = """Elias Dabbas"""
 __email__ = 'eliasdabbas@gmail.com'
 __version__ = '0.13.0'
 
-from advertools.ad_create import ad_create
-from advertools.ad_from_string import ad_from_string
-from advertools.emoji import *
-from advertools.extract import *
-from advertools.header_spider import crawl_headers
-from advertools.knowledge_graph import knowledge_graph
-from advertools.kw_generate import *
-from advertools.logs import (LOG_FIELDS, LOG_FORMATS, crawllogs_to_df,
+from advertools2.ad_create import ad_create
+from advertools2.ad_from_string import ad_from_string
+from advertools2.emoji import *
+from advertools2.extract import *
+from advertools2.header_spider import crawl_headers
+from advertools2.knowledge_graph import knowledge_graph
+from advertools2.kw_generate import *
+from advertools2.logs import (LOG_FIELDS, LOG_FORMATS, crawllogs_to_df,
                              logs_to_df)
-from advertools.regex import *
-from advertools.reverse_dns_lookup import reverse_dns_lookup
-from advertools.robotstxt import *
-from advertools.sitemaps import sitemap_to_df
-from advertools.spider import crawl
-from advertools.stopwords import stopwords
-from advertools.url_builders import url_utm_ga
-from advertools.urlytics import url_to_df
-from advertools.word_frequency import word_frequency
-from advertools.word_tokenize import word_tokenize
+from advertools2.regex import *
+from advertools2.reverse_dns_lookup import reverse_dns_lookup
+from advertools2.robotstxt import *
+from advertools2.sitemaps import sitemap_to_df
+from advertools2.spider import crawl
+from advertools2.stopwords import stopwords
+from advertools2.url_builders import url_utm_ga
+from advertools2.urlytics import url_to_df
+from advertools2.word_frequency import word_frequency
+from advertools2.word_tokenize import word_tokenize
 
 from . import twitter, youtube
 from .serp import *
